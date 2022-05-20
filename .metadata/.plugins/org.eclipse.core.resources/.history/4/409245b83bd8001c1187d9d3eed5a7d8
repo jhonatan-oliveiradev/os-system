e863f49ac04637dd079a9ffc650fe03b263b67f0
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-05-20 13:52:42 -0300

    initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4/409245b83bd8001c1187d9d3eed5a7d8
@@ -0,0 +1,11 @@
+package com.jhonatan.os.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jhonatan.os.domain.Tecnico;
+
+@Repository
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+
+}
---
+++ os/src/main/java/com/jhonatan/os/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.jhonatan.os.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jhonatan.os.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
